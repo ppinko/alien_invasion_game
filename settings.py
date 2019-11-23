e934ffea8d3a4dec96a64f559e5770b25f2a51c0
legpol <p.pinkowicz@gmail.com>
2019-11-23 12:35:23 +0100

    Adding class ship, updating screen settings

--- settings.py
+++ settings.py
@@ -5,5 +5,5 @@ class Settings():
         """Initilize the game's settings"""
         # Screen settingss
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
