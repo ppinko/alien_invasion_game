644732361bc711cdab78ead84ba5d1eef12d1763
legpol <p.pinkowicz@gmail.com>
2019-11-24 09:54:59 +0100

    Adding resetting and .gitignore

--- settings.py
+++ settings.py
@@ -20,8 +20,8 @@ class Settings():
         self.bullets_allowed = 3
 
         # Allien settings
-        self.alien_speed_factor = 1
-        self.fleet_drop_speed = 10
+        self.alien_speed_factor = 10
+        self.fleet_drop_speed = 30
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
 
